ff6b3e6a794d36c2278527374ce1929cc4966c72
Lionel <joechipjoechip@gmail.com>
2023-01-12 05:17:42 +0100

    add image background

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,11 @@ body {
     align-items: center;
     width: 100%;
     height: 100vh;
-    background-color: #000;
+    // background-color: #000;
+
+	background: url(https://images.unsplash.com/photo-1581610186406-5f6e9f9edbc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80);
+
+	background-size: cover;
 
   }
 }
