88a3ddf39d9fa3ea544ca6ab5897f8d1f133cc28
Lionel <joechipjoechip@gmail.com>
2023-01-14 05:16:54 +0100

    update json

--- src/components/CubesParty.vue
+++ src/components/CubesParty.vue
@@ -27,14 +27,27 @@ import InteractiveCube from "./InteractiveCube.vue"
 
 			:scroll-sensitive="true"
 			:scroll-anim-duration="0.5"
-			:scroll-anim-delta="15"
+			:scroll-anim-delta="7"
+
+			:permanent-rotation-increment="{ 
+				x: { angle: 6, duration: 150 },
+				y: { angle: 2, duration: 20 },
+				z: { angle: 10, duration: 300 }
+			}"
 		/>
 
 		<interactive-cube 
 			ref="cube" 
-			:mouse-sensitive="false"
+			:mouse-sensitive="true"
 			:scroll-sensitive="true"
 			:scroll-anim-duration="0.5"
+			:scroll-anim-delta="17"
+
+			:permanent-rotation-increment="{ 
+				x: { angle: 6, duration: 15 },
+				y: { angle: 90, duration: 20 },
+				z: { angle: 10, duration: 30 }
+			}"
 		/>
 
 		<interactive-cube 
