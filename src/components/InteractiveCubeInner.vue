138f27493aff434201a888b18cda9c9d2ba7f7f7
lio <joechipjoechip@gmail.com>
2023-01-19 12:19:41 +0100

    clean

--- src/components/InteractiveCubeInner.vue
+++ src/components/InteractiveCubeInner.vue
@@ -40,41 +40,6 @@ const props = defineProps({
 
 const store = inject("STORE")
 
-// const fragmentShader = `
-// uniform sampler2D myTextureOne;
-// uniform sampler2D myTextureTwo;
-// uniform float uProgress;
-
-// varying vec2 vUv;
-// varying float vScrollInfluence;
-
-// void main() {
-//   vec4 image1 = texture2D(myTextureOne, vUv);
-//   vec4 image2 = texture2D(myTextureTwo, vUv);
-//   gl_FragColor = mix(image1, image2, uProgress);
-// }
-// `
-
-// const vertexShader = `
-// uniform float uScrollInfluence;
-// uniform vec2 uMouse;
-
-// varying vec2 vUv;
-// varying float vScrollInfluence;
-
-// void main()
-// {
-// 	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-// 	vUv = uv;
-
-// 	vScrollInfluence = uScrollInfluence;
-
-// }
-// `
-// ici traitements vénère pour faire un truc cool avec vScrollInfluence
-
-
 
 
 // * * * * Content logic
